e6117cf220d0c3a4e61afe3955d2074a1b7a67a2
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-09-10 21:50:11 +0300

    Lemma form & quality support

--- mystem.go
+++ mystem.go
@@ -7,12 +7,11 @@ package mystem
 import "C"
 
 import (
+	"encoding/binary"
 	"unsafe"
 )
 
-import "encoding/binary"
-
-func StringToSymbols(word string) []C.ushort {
+func stringToSymbols(word string) []C.ushort {
 	var (
 		symbols []C.ushort
 	)
@@ -22,7 +21,7 @@ func StringToSymbols(word string) []C.ushort {
 	return symbols
 }
 
-func SymbolsToString(symbols *C.TSymbol, length C.int) string {
+func symbolsToString(symbols *C.TSymbol, length C.int) string {
 	var (
 		runes []rune
 	)
@@ -39,12 +38,8 @@ type Analyses struct {
 	handle unsafe.Pointer
 }
 
-type Lemma struct {
-	handle unsafe.Pointer
-}
-
 func NewAnalyses(word string) *Analyses {
-	cWord := StringToSymbols(word)
+	cWord := stringToSymbols(word)
 	cWordLength := len(cWord)
 	analyses := new(Analyses)
 	handle := C.MystemAnalyze((*C.TSymbol)(unsafe.Pointer(&cWord[0])), C.int(cWordLength))
@@ -52,7 +47,7 @@ func NewAnalyses(word string) *Analyses {
 	return analyses
 }
 
-func (analyses *Analyses) Length() int {
+func (analyses *Analyses) Count() int {
 	length := C.MystemAnalysesCount(analyses.handle)
 	return (int)(length)
 }
@@ -68,10 +63,41 @@ func (analyses *Analyses) GetLemma(i int) *Lemma {
 	return lemma
 }
 
+type Lemma struct {
+	handle unsafe.Pointer
+}
+
+// Lemma quality constants
+const (
+	Dictionary  = 0        // слово из словаря
+	Bastard     = 1        // не словарное
+	Sob         = 2        // из "быстрого словаря"
+	Prefixoid   = 4        // словарное + стандартный префикс (авто- мото- кино- фото-) всегда в компании с Bastard или Sob
+	Foundling   = 8        // непонятный набор букв, но проходящий в алфавит
+	BadRequest  = 16       // доп. флаг.: "плохая лемма" при наличии "хорошей" альтернативы ("махать" по форме "маша")
+	FromEnglish = 65536    // переведено с английского
+	ToEnglish   = 131072   // переведено на английский
+	Untranslit  = 262144   // "переведено" с транслита
+	Overrode    = 1048576  // текст леммы был перезаписан
+	Fix         = 16777216 // слово из фикс-листа
+)
+
 func (lemma *Lemma) TextLength() C.int {
 	return C.MystemLemmaTextLen(lemma.handle)
 }
 
 func (lemma *Lemma) Text() string {
-	return SymbolsToString(C.MystemLemmaText(lemma.handle), lemma.TextLength())
+	return symbolsToString(C.MystemLemmaText(lemma.handle), lemma.TextLength())
+}
+
+func (lemma *Lemma) FormLength() C.int {
+	return C.MystemLemmaFormLen(lemma.handle)
+}
+
+func (lemma *Lemma) Form() string {
+	return symbolsToString(C.MystemLemmaForm(lemma.handle), lemma.FormLength())
+}
+
+func (lemma *Lemma) Quality() int {
+	return int(C.MystemLemmaQuality(lemma.handle))
 }

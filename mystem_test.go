e60c7bdd87e724c35d174a441891fe4f0e94b5a6
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-09-10 23:15:34 +0300

    Initial tests

---
+++ mystem_test.go
@@ -0,0 +1,91 @@
+package mystem
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetAnalysesCount(t *testing.T) {
+	analyses := NewAnalyses("маша")
+	defer analyses.Close()
+	if analyses.Count() != 2 {
+		t.Fail()
+	}
+}
+
+func TestGetLemmaText(t *testing.T) {
+	analyses := NewAnalyses("маша")
+	defer analyses.Close()
+	lemma := analyses.GetLemma(0)
+	if lemma.TextLength() != 4 {
+		t.Fail()
+	}
+	if lemma.Text() != "маша" {
+		t.Fail()
+	}
+	lemma = analyses.GetLemma(1)
+	if lemma.TextLength() != 6 {
+		t.Fail()
+	}
+	if lemma.Text() != "махать" {
+		t.Fail()
+	}
+}
+
+func TestGetLemmaForm(t *testing.T) {
+	analyses := NewAnalyses("маша")
+	defer analyses.Close()
+	lemma := analyses.GetLemma(0)
+	if lemma.FormLength() != 4 {
+		t.Fail()
+	}
+	if lemma.Form() != "маша" {
+		t.Fail()
+	}
+}
+
+func TestGetLemmaQuality(t *testing.T) {
+	analyses := NewAnalyses("маша")
+	lemma := analyses.GetLemma(0)
+	if lemma.Quality() != Dictionary {
+		t.Fail()
+	}
+	analyses.Close()
+	analyses = NewAnalyses("майстем")
+	defer analyses.Close()
+	lemma = analyses.GetLemma(0)
+	if lemma.Quality() != Bastard {
+		t.Fail()
+	}
+}
+
+func TestGetLemmaStemGrammemes(t *testing.T) {
+	analyses := NewAnalyses("маша")
+	defer analyses.Close()
+	lemma := analyses.GetLemma(0)
+	grammemes := lemma.StemGram()
+	if !reflect.DeepEqual(grammemes, []int{Substantive, FirstName, Feminine, Animated}) {
+		t.Fail()
+	}
+}
+
+func TestGetLemmaFlexGrammemes(t *testing.T) {
+	analyses := NewAnalyses("маша")
+	defer analyses.Close()
+	lemma := analyses.GetLemma(0)
+	grammemes := lemma.FlexGram()
+	if !reflect.DeepEqual(grammemes, [][]int{[]int{Nominative, Singular}}) {
+		t.Fail()
+	}
+}
+
+func BenchmarkMystem(b *testing.B) {
+	mystem := NewAnalyses("маша")
+	defer mystem.Close()
+	for i := 0; i < b.N; i++ {
+		for i := 0; i < mystem.Count(); i++ {
+			lemma := mystem.GetLemma(i)
+			lemma.Text()
+		}
+	}
+}
